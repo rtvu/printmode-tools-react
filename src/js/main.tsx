1f7490ea32e29e95840694ad797a9d9bfcc308f2
ryan <rtvu87@gmail.com>
2022-09-04 20:28:37 -0700

    Added tailwindcss

--- src/js/main.tsx
+++ src/js/main.tsx
@@ -1,3 +1,3 @@
 import { render } from "react-dom";
 
-render(<h1>Printmode Tools</h1>, document.getElementById("application"));
+render(<h1 className="text-6xl">Printmode Tools</h1>, document.getElementById("application"));
